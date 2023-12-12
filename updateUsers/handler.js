52673f799aee345a4096712670deacf1da136f29
martin <martin_rps_9819@outlook.com>
2023-12-12 04:33:08 -0500

    Agregando todos los archivos para cargarlos a github

---
+++ updateUsers/handler.js
@@ -0,0 +1,38 @@
+const aws = require("aws-sdk")
+
+let dynamoDbClientParams = {}
+
+
+
+const dynamodb = new aws.DynamoDB.DocumentClient(dynamoDbClientParams)
+
+
+const updateUsers = async (event, context) => {
+
+    let userId = event.pathParameters.id
+
+    const body = JSON.parse(event.body)
+    const params = {
+        TableName: 'usersTable',
+        Key: { pk: userId },
+        UpdateExpression: 'set #name = :name , #telefono = :telefono',
+        ExpressionAttributeNames: { '#name': 'name', '#telefono': 'telefono' },
+        ExpressionAttributeValues: { ':name': body.name, ":telefono": body.telefono },
+        ReturnValues: "ALL_NEW"
+    }
+
+    return dynamodb.update(params)
+        .promise()
+        .then(res => {
+            console.log(res)
+            return {
+                "statusCode": 200,
+                "body": JSON.stringify({ "user": res })
+            }
+        })
+}
+
+
+module.exports = {
+    updateUsers
+}
